a14c214abde7375ad543efaa65ceff2d5534e672
Patrick Michaud <pmichaud@uw.edu>
2017-03-02 08:49:00 -0800

    using pypi restclients, deploying to pypi

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-                      'UW-RestClients-Core==0.1.2'
+                      'UW-RestClients-Core==0.1.3'
                      ],
     license='Apache License, Version 2.0',
     description=('A library for connecting to the Person Web Service at the '
