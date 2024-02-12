2055f0a8c53233705f6fba523ee46734cb9c190f
Jim Laney <jlaney@uw.edu>
2024-02-11 20:24:40 -0800

    add python test matrix, enable deprecation warnings

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name='UW-RestClients-PWS',
     version=VERSION,
     packages=['uw_pws'],
-    author="UW-IT AXDD",
+    author="UW-IT T&LS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
@@ -38,7 +38,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
     ],
 )
 
